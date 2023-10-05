34945eaa663d9617a8de237534738945cbcdc861
Cro0ky <143035135+Cro0ky@users.noreply.github.com>
2023-10-05 23:55:02 +0300

    Add files via upload

---
+++ repnin_les2.py
@@ -0,0 +1,88 @@
+# a = input()
+# print(a[2])
+# print(a[-2])
+# print(a[:5])
+
+# print(a[0::2])
+# print(a[1::2])
+# print(a[::-1])
+# a = a[::-1]
+# print(a[0::2])
+# print(len(a))
+#task 2
+# text = input()
+# print(text.find(' ') + 1)
+#task 3
+# str1 = input()
+# b = len(str1) // 2
+# print(slice(str1[b]))
+#task4
+# str2 = input()
+# print(str2.find('f'))
+# print(str2.rfind('f'))
+#task 7
+# str3 = input()
+# print(str3.replace('1', 'one'))
+#task 8
+# str4 = input()
+# print(str4.replace('@', ''))
+# task
+# straka = input()
+# a = straka.replace('f', '', 1)
+# b = a[::-1]
+# c = b.replace('f', '', 1)
+# print(c[::-1])
+#task 9
+# num1 = int(input('1n'))
+# num2 = int(input('2n'))
+# if num1 > num2:
+#     print(num1)
+# else:
+#     print(num2)
+#task 10
+# x = int(input('1n'))
+# if x > 0:
+#     print('sign = 1')
+# elif x < 0:
+#     print('sign = -1')
+# else:
+#     print('sign = 0')
+#task 11
+# x1 = int(input('x1'))
+# x2 = int(input('x2'))
+# y1 = int(input('y1'))
+# y2 = int(input('y2'))
+# if 1 <= x1 <= 8 and 1 <= x2 <= 8 and 1 <= y1 <= 8 and 1 <= y2 <= 8:
+#     if (x1+ y1) % 2 == 0 and (x2 + y2) % 2 == 0:
+#         print('yes')
+#     else:
+#         print('no')
+# else:
+#     print('no')
+#task 12
+# year = int(input('year: '))
+# if year % 4 == 0 and year % 100 != 0 and year % 400 == 0:
+#     print('Год весакотный')
+# else:
+#     print("Год не весакостный")
+#task 13
+# a1 = int(input())
+# a2 = int(input())
+# a3 = int(input())
+#
+# if a1 == a2 == a3:
+#     print(3)
+# elif a1 == a2 or a2 == a3 or a1 == a3:
+#     print(2)
+# else:
+#     print(0)
+#task 14
+# n = int(input('n'))
+# m = int(input('m'))
+# k = int(input('k'))
+#
+# if (n * m) > k:
+#     print(True)
+# else:
+#     print(False)
+
